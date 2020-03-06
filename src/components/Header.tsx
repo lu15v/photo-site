a35585c52c8a4f99bec41004d3abc0cb31bc69dc
lu15v <lu15bytes@gmail.com>
2020-03-06 13:43:34 -0800

    MOD: changed to tsx syntax

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles(theme => ({
     },
   }));
 
-const Header = () => {
+const Header: React.FC<{}> = () => {
     const classes = useStyles();
     return(
         <AppBar position="static" className={classes.appBarColor}>
