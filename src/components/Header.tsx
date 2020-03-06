b9150e87bb8a99c88dddabc6d56febc057ea59c4
lu15v <lu15bytes@gmail.com>
2020-03-06 15:50:25 -0800

    FEATURE: sticky header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,12 @@ const useStyles = makeStyles(theme => ({
     root: {
       flexGrow: 1,
     },
-    appBarColor:{
+    appBar:{
         //background: rgb(2,0,36);
-        background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(255,0,24,1) 100%)'
+        background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(255,0,24,1) 100%)',
+        position: 'sticky',
+        top: '0',
+        marginBottom: '10px'
     },
     menuButton: {
       marginRight: theme.spacing(2),
@@ -22,7 +25,7 @@ const useStyles = makeStyles(theme => ({
 const Header: React.FC<{}> = () => {
     const classes = useStyles();
     return(
-        <AppBar position="static" className={classes.appBarColor}>
+        <AppBar  className={classes.appBar}>
         <Toolbar>
           <IconButton edge="start" color="inherit" className={classes.menuButton} aria-label="menu">
           
