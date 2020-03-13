661c6d2fc192c259100aa0ca868171850256f775
lu15v <lu15bytes@gmail.com>
2020-03-12 22:41:47 -0700

    STYLE: for the app and the tempDrawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import CardContainer from './containers/CardContainer';
+import {ImageData} from './typings';
+import Footer from './components/Footer';
+
+const cards: ImageData[] = [
+  {title: "picture 1", img: "https://i.picsum.photos/id/159/500/250.jpg", author: "author", cols: 3},
+  {title: "picture 2", img: "https://i.picsum.photos/id/151/500/250.jpg",  author: "author", cols: 2},
+  {title: "picture 3", img: "https://i.picsum.photos/id/156/300/450.jpg",  author: "author", cols: 1},
+  {title: "picture 1", img: "https://i.picsum.photos/id/159/500/250.jpg", author: "author", cols: 3},
+  {title: "picture 2", img: "https://i.picsum.photos/id/151/500/250.jpg",  author: "author", cols: 2},
+  {title: "picture 3", img: "https://i.picsum.photos/id/156/300/450.jpg",  author: "author", cols: 1},
+  {title: "picture 1", img: "https://i.picsum.photos/id/159/500/250.jpg", author: "author", cols: 3},
+  {title: "picture 2", img: "https://i.picsum.photos/id/151/500/250.jpg",  author: "author", cols: 2},
+  {title: "picture 3", img: "https://i.picsum.photos/id/156/300/450.jpg",  author: "author", cols: 1},
+  {title: "picture 1", img: "https://i.picsum.photos/id/159/500/250.jpg", author: "author", cols: 3},
+  {title: "picture 2", img: "https://i.picsum.photos/id/151/500/250.jpg",  author: "author", cols: 2},
+  {title: "picture 3", img: "https://i.picsum.photos/id/156/300/450.jpg",  author: "author", cols: 1},
+  {title: "picture 1", img: "https://i.picsum.photos/id/159/500/250.jpg", author: "author", cols: 3},
+  {title: "picture 2", img: "https://i.picsum.photos/id/151/500/250.jpg",  author: "author", cols: 2},
+  {title: "picture 3", img: "https://i.picsum.photos/id/156/300/450.jpg",  author: "author", cols: 1},
+  {title: "picture 1", img: "https://i.picsum.photos/id/159/500/250.jpg", author: "author", cols: 3},
+  {title: "picture 2", img: "https://i.picsum.photos/id/151/500/250.jpg",  author: "author", cols: 2},
+  {title: "picture 3", img: "https://i.picsum.photos/id/156/300/450.jpg",  author: "author", cols: 1},
+  {title: "picture 1", img: "https://i.picsum.photos/id/159/500/250.jpg", author: "author", cols: 3},
+  {title: "picture 2", img: "https://i.picsum.photos/id/151/500/250.jpg",  author: "author", cols: 2},
+  {title: "picture 3", img: "https://i.picsum.photos/id/156/300/450.jpg",  author: "author", cols: 1},
+  {title: "picture 1", img: "https://i.picsum.photos/id/159/500/250.jpg", author: "author", cols: 3},
+  {title: "picture 2", img: "https://i.picsum.photos/id/151/500/250.jpg",  author: "author", cols: 2},
+  {title: "picture 3", img: "https://i.picsum.photos/id/156/300/450.jpg",  author: "author", cols: 1}
+]
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <CardContainer contentList={cards}/>
+      <Footer/>
     </div>
   );
 }
--- src/components/TempDrawer.tsx
+++ src/components/TempDrawer.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 
 const drawerWidth = 240;
 
@@ -25,26 +19,15 @@ const useStyles = makeStyles((theme: Theme) =>
         duration: theme.transitions.duration.leavingScreen,
       }),
     },
-    appBarShift: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    hide: {
-      display: 'none',
-    },
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
     drawerPaper: {
       width: drawerWidth,
+      background: '#424242'
+    },
+    fontColor: {
+      color: "white"
+    },
+    divider :{
+      backgroundColor: "white"
     },
     drawerHeader: {
       display: 'flex',
@@ -52,22 +35,6 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: theme.spacing(0, 1),
       ...theme.mixins.toolbar,
       justifyContent: 'flex-end',
-    },
-    content: {
-      flexGrow: 1,
-      padding: theme.spacing(3),
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      marginLeft: -drawerWidth,
-    },
-    contentShift: {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
     },
   }),
 );
@@ -81,37 +48,19 @@ const TempDrawer: React.FC<DrawerProps> = (props) => {
 
   return (
       <Drawer
-        className={classes.drawer}
         variant="persistent"
         anchor="left"
         open={props.open}
         classes={{
-          paper: classes.drawerPaper,
+          paper: classes.drawerPaper
         }}
       >
         <div className={classes.drawerHeader}>
-          <IconButton onClick={props.drawerClose}>
+          <IconButton  className={classes.fontColor} onClick={props.drawerClose}>
             {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
           </IconButton>
         </div>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+        <Divider className={classes.divider}/>
       </Drawer>
   );
 }
