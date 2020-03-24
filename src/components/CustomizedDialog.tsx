5797129b460f5493d588a1c6b31c03f594c83776
lu15v <lu15bytes@gmail.com>
2020-03-23 22:05:53 -0700

    MOD: adapted component for showing picture

--- src/components/CustomizedDialog.tsx
+++ src/components/CustomizedDialog.tsx
@@ -7,6 +7,7 @@ import MuiDialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -70,19 +71,9 @@ const CustomizedDialog: React.FC<CustomDialogProps> = (props) => {
           Modal title {id}
         </DialogTitle>
         <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-            lacus vel augue laoreet rutrum faucibus dolor auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-            auctor fringilla.
-          </Typography>
+        <CardMedia>
+          <img src="https://i.picsum.photos/id/156/300/450.jpg"/>
+        </CardMedia>
         </DialogContent>
         <DialogActions>
         </DialogActions>
